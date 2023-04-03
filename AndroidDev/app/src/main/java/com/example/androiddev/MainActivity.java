d037ba7e0638c8b52f1412da2cc77d5ca9d637a9
Thanasis Emmanouilidis <p19emma@ionio.gr>
2023-04-03 23:12:14 +0300

    Login_Page

--- AndroidDev/app/src/main/java/com/example/androiddev/MainActivity.java
+++ AndroidDev/app/src/main/java/com/example/androiddev/MainActivity.java
@@ -2,7 +2,14 @@ package com.example.androiddev;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.res.XmlResourceParser;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.xmlpull.v1.XmlPullParser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void registerLink(View view){
+        Intent intent = new Intent(MainActivity.this, ThirdActivity.class);
+        startActivity(intent);
     }
 }
