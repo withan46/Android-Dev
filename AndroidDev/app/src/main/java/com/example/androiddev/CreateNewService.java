da44bf6385fa7ab037b8363500c446a9d3a55d10
Prodromos Nasis <prodromos.nasis@gmail.com>
2023-04-06 16:59:13 +0300

    background_added

---
+++ AndroidDev/app/src/main/java/com/example/androiddev/CreateNewService.java
@@ -0,0 +1,17 @@
+package com.example.androiddev;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateNewService extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_new_service);
+    }
+}
