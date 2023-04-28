ef9123d92218bfd7ccef8e009b689fb4d40e1ff5
PaulosMpou <paulosmpou02@gmail.com>
2023-04-28 18:30:00 +0300

    r7 UI v1

---
+++ AndroidDev/app/src/main/java/com/example/androiddev/R7.java
@@ -0,0 +1,14 @@
+package com.example.androiddev;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class R7 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.r7layout);
+    }
+}
