fa530fbdeefc6d4c4a301b70e01323c5a8a60ba3
Dimitrios Komitis <105862034+komitisDM@users.noreply.github.com>
2023-05-21 16:24:26 +0100

    Changed Naming of Classes
    This should be the final individual version...

--- AndroidDev/app/src/main/java/com/example/androiddev/MainActivity.java
+++ AndroidDev/app/src/main/java/com/example/androiddev/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void registerLink(View view){
-        Intent intent = new Intent(MainActivity.this, R8.class);
+        Intent intent = new Intent(MainActivity.this, AddDescriptionR8.class);
         startActivity(intent);
 
     }
