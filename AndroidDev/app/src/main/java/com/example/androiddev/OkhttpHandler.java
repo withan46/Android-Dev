4026efcacc418573f0eb08a2524ab0a697922638
PaulosMpou <paulosmpou02@gmail.com>
2023-05-21 19:02:28 +0300

    Database Deployment

---
+++ AndroidDev/app/src/main/java/com/example/androiddev/OkhttpHandler.java
@@ -0,0 +1,133 @@
+package com.example.androiddev;
+
+import android.os.StrictMode;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import okhttp3.FormBody;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class OkhttpHandler {
+
+
+    public OkhttpHandler() {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+    }
+
+    public OkHttpClient customSSL() throws KeyManagementException, NoSuchAlgorithmException{
+        TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+            public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+            }
+
+            public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+            }
+
+            public X509Certificate[] getAcceptedIssuers() {
+                return new X509Certificate[0];
+            }
+        }};
+
+        // Create a SSL context with the trust manager
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, trustAllCerts, new SecureRandom());
+
+        // Configure OkHttpClient to use the custom SSL context
+        OkHttpClient client = new OkHttpClient.Builder()
+                .sslSocketFactory(sslContext.getSocketFactory(), (X509TrustManager) trustAllCerts[0])
+                .hostnameVerifier((hostname, session) -> true)
+                .build();
+        return client;
+    }
+
+    public void fetchData(ArrayList<Appointment> appointmentsList) throws IOException, KeyManagementException, NoSuchAlgorithmException {
+        String appointments = "no data";
+        String url = "https://192.168.1.5/ViewAndManageAppointmentRequestsR7/fetchAppointmentDetails.php";
+
+        // Create a trust manager that accepts all certificates
+
+
+        RequestBody body = RequestBody.create("", MediaType.parse("text/plain"));
+        Request request = new Request.Builder().url(url).method("POST", body).build();
+        //Fetching the data from the database
+        Response response = customSSL().newCall(request).execute();
+        if (response.isSuccessful())
+            appointments = response.body().string();
+        try{
+            //Storing every appointment's information in variables.
+            JSONArray jsonAppointments = new JSONArray(appointments);
+            for(int i =0 ; i < jsonAppointments.length() ; i ++){
+                JSONObject patientInformation = jsonAppointments.getJSONObject(i);
+                String patientSSN = patientInformation.getString("ssn");
+                String patientName = patientInformation.getString("name");
+                String appointmentDate = patientInformation.getString("date");
+                String appointmentTime = patientInformation.getString("time");
+                String appointmentNote = patientInformation.getString("tos");
+                int appointmentID = patientInformation.getInt("id");
+                //Creating a new appointment object, based on the information of the current appointment.
+                Appointment appointment = new Appointment(patientSSN, patientName, appointmentDate, appointmentTime, appointmentNote,appointmentID);
+                //Adding the appointment to the appointments' list.
+                appointmentsList.add(appointment);
+
+
+            }
+            response.body().close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public void onAcceptClicked(int appointmentID){
+            String url = "https://192.168.1.5/ViewAndManageAppointmentRequestsR7/acceptAppointment.php";
+
+            RequestBody requestBody = new FormBody.Builder().add("appointment_id", String.valueOf(appointmentID)).build();
+            Request request = new Request.Builder().url(url).post(requestBody).build();
+
+            try{
+                // changing the state of an appointment from false to true.
+                Response response = customSSL().newCall(request).execute();
+                response.body().close();
+            }catch(IOException | KeyManagementException | NoSuchAlgorithmException e){
+                e.printStackTrace();
+            }
+
+
+
+    }
+
+    public void onDenyClicked(int appointmentID){
+        String url = "https://192.168.1.5/ViewAndManageAppointmentRequestsR7/denyAppointment.php";
+
+        RequestBody requestBody = new FormBody.Builder().add("appointment_id", String.valueOf(appointmentID)).build();
+        Request request = new Request.Builder().url(url).post(requestBody).build();
+
+        try{
+            // deleting an appointment from the database.
+            Response response = customSSL().newCall(request).execute();
+            response.body().close();
+        }catch(IOException | KeyManagementException | NoSuchAlgorithmException e){
+            e.printStackTrace();
+        }
+    }
+
+
+}
---
+++ AndroidDev/app/src/main/java/com/example/androiddev/ViewAndManageAppointmentRequestsR7.java
@@ -0,0 +1,104 @@
+package com.example.androiddev;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class ViewAndManageAppointmentRequestsR7 extends AppCompatActivity {
+    ArrayList<Appointment> appointments = new ArrayList<>();
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        //Creating an OkhttpHandler object to manage any communication with the database.
+
+        OkhttpHandler handler = new OkhttpHandler();
+        try {
+            handler.fetchData(appointments);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_and_manage_appointment_requests_r7);
+
+        // Fetching the parent of the appointment container, inside which an appointment's information will be visible.
+        LinearLayout appointmentContainerParent = findViewById(R.id.patientsParent);
+
+         for(int i = 0 ; i < appointments.size() ; i++){
+
+             // Getting the layout needed, in order to add appointment containers inside the currently visible layout.
+             LayoutInflater inflater = LayoutInflater.from(this);
+             LinearLayout appointmentContainer = (LinearLayout) inflater.inflate(R.layout.appointment_container_layout_r7, appointmentContainerParent, false);
+
+
+
+
+
+             // fetching the elements that vary and change dynamically based on the appointment.
+             TextView name = appointmentContainer.findViewById(R.id.patientName);
+             TextView date = appointmentContainer.findViewById(R.id.appDate);
+             TextView time = appointmentContainer.findViewById(R.id.appTime);
+             TextView note = appointmentContainer.findViewById(R.id.note);
+
+
+             // getting all the necessary data and adding it to the appropriate elements.
+             Appointment anAppointment = appointments.get(i);
+             // setting the container's id as the appointment id. It is necessary in order to be able to perform any actions regarding that appointment, before removing it from the list.
+             appointmentContainer.setTag(anAppointment.getAppointment_id());
+             //Adding the necessary information inside the appointment container.
+             name.setText(anAppointment.getPatient_name());
+             date.setText(anAppointment.getDate());
+             time.setText(anAppointment.getTime());
+             note.setText(anAppointment.getTos());
+
+
+             // Adding the layout containing an appointment's information to its parent layout.
+
+             appointmentContainerParent.addView(appointmentContainer);
+
+             //Getting the accept and deny appointment buttons, in order to add onClickListeners.
+             Button acceptButton = appointmentContainer.findViewById(R.id.acceptButton);
+             Button denyButton = appointmentContainer.findViewById(R.id.denyButton);
+
+             acceptButton.setOnClickListener(new View.OnClickListener(){
+                 @Override
+                 public void onClick(View v){
+                     // Getting the patientBox layout (Buttons are each inside a relative layout, which is inside a linear layout, which is inside the patientBox)
+                     LinearLayout parent = (LinearLayout) acceptButton.getParent().getParent().getParent();
+                     //Calling the onAcceptClicked method of the handler object that changes the state of the appointment from false to true.
+                     handler.onAcceptClicked((int) parent.getTag());
+
+                     //Removing the appointment container after the aforementioned action has been completed.
+                     parent.setVisibility(View.GONE);
+                 }
+             });
+
+             denyButton.setOnClickListener(new View.OnClickListener(){
+                 @Override
+                 public void onClick(View v){
+                     // Getting the patientBox layout (Buttons are each inside a relative layout, which is inside a linear layout, which is inside the patientBox)
+                     LinearLayout parent = (LinearLayout) denyButton.getParent().getParent().getParent();
+                     //Calling the onDenyClicked method of the handler object that deletes the appointment from the database.
+                     handler.onDenyClicked((int) parent.getTag());
+                     //Removing the appointment container after the aforementioned action has been completed.
+                     parent.setVisibility(View.GONE);
+                 }
+             });
+
+         }
+
+    }
+    
+}
